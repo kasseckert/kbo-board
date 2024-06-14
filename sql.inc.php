84378c2149f7e50aa218808602eb3589897e993a
Christoph Kasseckert <128835492+kasseckert@users.noreply.github.com>
2024-06-14 17:07:00 +0200

    Add files via upload

---
+++ sql.inc.php
@@ -0,0 +1,18 @@
+<?php
+    error_reporting(E_ALL);
+    define ('MYSQL_HOST','Host');
+    define ('MYSQL_BENUTZER','Benutername');
+    define ('MYSQL_KENNWORT','Passwort');
+    define ( 'MYSQL_DATENBANK','Datenbankname');
+    $db_link = mysqli_connect (MYSQL_HOST, 
+                            MYSQL_BENUTZER, 
+                            MYSQL_KENNWORT, 
+                            MYSQL_DATENBANK);
+    mysqli_set_charset($db_link, 'utf8');
+    // Tiny API Key
+    $tinykey = '';
+    // Einbindung Bootstrap im Header
+    $bootstrap_css = '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">';
+    $bootstrap_js = '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>';
+    $bootstrap_icons = '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">';
+?>
